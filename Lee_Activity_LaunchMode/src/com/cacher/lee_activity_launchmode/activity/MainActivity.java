11c18090f3c7797f33acc86355cc3d4f0e478c30
Cacle <lm455261979@163.com>
2015-06-03 21:19:54 +0800

    andorid启动模式

---
+++ Lee_Activity_LaunchMode/src/com/cacher/lee_activity_launchmode/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cacher.lee_activity_launchmode.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
